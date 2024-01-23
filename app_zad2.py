b824cf0a5e9442706e1a0430b26aaba03564ec11
Michał Gabryelski <micgab001@pbs.edu.pl>
2024-01-23 12:40:10 +0100

    poprawa app_zad2.py

--- app_zad2.py
+++ app_zad2.py
@@ -1,10 +1,16 @@
-def add_numbers(a, b):
+def add_numbers(a , b):
     return a+b
+
+
 def subtract_numbers(a , b):
     return a - b
-def multiply_numbers(a,b):
+
+
+def multiply_numbers(a , b):
     return a* b
-def divide_numbers(a, b):
+
+
+def divide_numbers(a , b):
     if b== 0:
         raise ValueError("Dzielenie przez zero jest niedozwolone.")
     return a /b
